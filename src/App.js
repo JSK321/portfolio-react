5693c3209087bde74f387d2007be5ce86586d6c9
Jae Kim <jaeshinkim321@gmail.com>
2020-11-10 18:00:35 -0800

    added contact me page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar"
 import AboutMe from "./components/AboutMe"
+import Contact from "./components/Contact"
 
 
 function App() {
@@ -14,7 +15,9 @@ function App() {
         <Route exact path="/aboutme">
           <AboutMe />
         </Route>
-
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
       </Switch>
     </Router>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import "./styles.css"
 
 export default function Contact() {
     return (
-        <div>
-            
+        <div className="container">
+            <h1>Contact</h1>
+            <hr />
+            <form>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label for="inputName">Name</label>
+                        <input type="name" className="form-control" id="inputName" placeholder="Name" />
+                    </div>
+
+                    <div className="form-group col-md-6">
+                        <label for="inputEmail">Email</label>
+                        <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label for="formControlTextarea">Message</label>
+                    <textarea className="form-control" id="formControlTextarea" rows="3" placeholder="Message"></textarea>
+                </div>
+
+                <div className="form-group text-center">
+                    <button type="submit" className="btn btn-dark contact">Submit</button>
+                </div>
+
+            </form>
         </div>
     )
 }
