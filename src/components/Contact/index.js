f4a81fee233cd0d5d03b1231cd8dd71082073966
Jae Kim <jaeshinkim321@gmail.com>
2021-02-11 18:41:16 -0800

    removed font awesome

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,43 @@
 import React from 'react'
 import { Card, Button, Row, Col } from 'react-bootstrap'
+import './styles.css'
 
 export default function Contact() {
     return (
         <Card>
             <h1>Contact me</h1>
-            <Button href="https://www.linkedin.com/in/jae-kim-31a56b1b5/" target="_blank" >Linkedin</Button>
-            <Button href="https://github.com/JSK321" target="_blank" >GitHub</Button>
-            <Button href="https://docs.google.com/document/d/1DJj88-hv__aqLyTUZuTATGz8fmnwqgsl356eMAdK38c/edit" target="_blank" >Resume</Button>
-            <p>Email: jaeshinkim321@gmail.com</p>
-            <p>Phone: 253-292-7744</p>
+            <Row className="skillsRow">
+                <Col>
+                    <Button
+                        href="https://www.linkedin.com/in/jae-kim-31a56b1b5/"
+                        target="_blank" >
+                        Linkedin
+                    </Button>
+                </Col>
+                <Col>
+                    <Button
+                        href="https://github.com/JSK321"
+                        target="_blank"
+                    >
+                        GitHub
+                    </Button>
+                </Col>
+                <Col>
+                    <Button
+                        href="https://docs.google.com/document/d/14tQljuyZufhgLDpHjVz9Lp3D3-OV7fbjQKA-8Inv-KM/edit?usp=sharing"
+                        target="_blank" >
+                        Resume
+                    </Button>
+                </Col>
+            </Row>
+            <Row className="contactRow">
+                <Card.Text>
+
+                : jaeshinkim321@gmail.com
+
+                : 253-292-7744
+            </Card.Text>
+            </Row>
         </Card>
     )
 }
